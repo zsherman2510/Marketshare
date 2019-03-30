e1407a3a17f71f05b1c19521c92944d73d57e0e2
zavion sherman <zsherman2510@hotmail.com>
2019-03-30 16:38:10 -0700

    created search functionality for all search possibilities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import * as serviceWorker from './serviceWorker';
 // Bring in default Element React theme
-import "element-theme-default";
+import 'element-theme-default';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+Amplify.configure(aws_exports);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,11 +1,95 @@
 import React from "react";
-// import { Loading, Tabs, Icon } from "element-react";
-
+import { API, graphqlOperation } from "aws-amplify";
+import { getMarket } from "../graphql/queries";
+import { Loading, Tabs, Icon } from "element-react";
+import { Link } from "react-router-dom";
+import NewProduct from "../components/NewProduct";
+import Product from "../components/Product";
 class MarketPage extends React.Component {
-  state = {};
+  state = {
+    market: null,
+    isLoading: true,
+    isMarketOwner: false
+  };
+
+  componentDidMount() {
+    this.handleGetMarket();
+  }
+
+  handleGetMarket = async () => {
+    const input = {
+      id: this.props.marketId
+    };
+    const results = await API.graphql(graphqlOperation(getMarket, input));
+    console.log({ results });
+    this.setState({ market: results.data.getMarket, isLoading: false }, () => {
+      this.checkMarketOwner();
+    });
+  };
+
+  checkMarketOwner = () => {
+    const { user } = this.props;
+    const { market } = this.state;
+    if (user) {
+      this.setState({ isMarketOwner: user.username === market.owner });
+    }
+  };
 
   render() {
-    return <div>MarketPage</div>;
+    const { market, isLoading, isMarketOwner } = this.state;
+    return isLoading ? (
+      <Loading fullscreen={true} />
+    ) : (
+      <>
+        {/* back button */}
+        <Link className="link" to="/">
+          Back to Markets List
+        </Link>
+
+        {/* Market MetaData */}
+        <span className="items-center pt-2">
+          <h2 className="mb-mr">{market.name}</h2>- {market.owner}
+        </span>
+        <div className="items-center pt-2">
+          <span style={{ color: "var(--lightSquidInk)", paddingBottom: "1em" }}>
+            <Icon name="date" className="icon" />
+            {market.createdAt}
+          </span>
+        </div>
+        {/* New Product */}
+        <Tabs type="border-card" value={isMarketOwner ? "1" : "2"}>
+          {isMarketOwner && (
+            <Tabs.Pane
+              label={
+                <>
+                  <Icon name="plus" className="icon" />
+                  Add Product
+                </>
+              }
+              name="1"
+            >
+              <NewProduct />
+            </Tabs.Pane>
+          )}
+          {/* Products List */}
+          <Tabs.Pane
+            label={
+              <>
+                <Icon name="menu" className="icon" />
+                Products ({market.products.items.length})
+              </>
+            }
+            name="2"
+          >
+            {/* <div className="product-list">
+                {market.products.items.map(product => (
+                  <Product product={product} />
+                ))}
+              </div> */}
+          </Tabs.Pane>
+        </Tabs>
+      </>
+    );
   }
 }
 
